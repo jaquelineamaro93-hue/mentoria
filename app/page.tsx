766cd932a23b3cab32caad25f0177ce77bd85965
Jaqueline Amaro <jaqueline.amaro93@gmail.com>
2026-07-28 20:14:48 -0300

    remove: delete static page

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '50vh' }}>
-      <h1>Redirecionando...</h1>
-      <script>{`window.location.href = '/auth'`}</script>
-    </div>
-  );
-}
