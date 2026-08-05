5500a147a6902341540375522803079ba006c6d5
Jaqueline Amaro <jaqueline.amaro93@gmail.com>
2026-08-05 00:59:34 -0300

    Update route.ts

--- app/api/admin/reset-password/route.ts
+++ app/api/admin/reset-password/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: Request) {
       type: 'recovery',
       email: email,
       options: {
-        ,
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'https://somamentoria.com'}/reset-password`,
       },
     });
 
